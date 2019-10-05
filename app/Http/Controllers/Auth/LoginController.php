e4466ed48c9b101923296fd799e80c708ce94046
blt950 <blt950@users.noreply.github.com>
2019-10-05 17:39:27 +0200

    Started on integrating vatsim sso

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,39 +1,96 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
-
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Vatsim\OAuth\SSOException;
+use Vatsim\OAuth\SSO;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\RatingsController;
+use App\Http\Controllers\PRatingsController;
+use App\Http\Controllers\DivisionsController;
+use App\Http\Controllers\TestController;
+use App\Models\User;
+/**
+ * Class AuthController
+ * @package App\Http\Controllers\login
+ */
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
-
     /**
-     * Where to redirect users after login.
-     *
-     * @var string
+     * @var SSO
      */
-    protected $redirectTo = '/home';
-
+    private $sso;
     /**
-     * Create a new controller instance.
-     *
-     * @return void
+     * LoginController constructor.
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->sso = new SSO(
+            config('sso.base'),
+            config('sso.key'),
+            config('sso.secret'),
+            config('sso.method'),
+            config('sso.cert'),
+            config('sso.additionalConfig')
+        );
+    }
+    /**
+     * Redirect user to VATSIM SSO for login
+     *
+     * @throws \Vatsim\OAuth\SSOException
+     */
+    public function login()
+    {
+        $this->sso->login(config('sso.return'), function ($key, $secret, $url) {
+            session()->put('key', $key);
+            session()->put('secret', $secret);
+            session()->save();
+            header('Location: ' . $url);
+            die();
+        });
+    }
+    /**
+     * Validate the login and access protected resources, create the user if they don't exist, update them if they do, and log them in
+     *
+     * @param Request $get
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @throws \Vatsim\OAuth\SSOException
+     */
+    public $newUser;
+    public function validateLogin(Request $get)
+    {
+        try{
+            $this->sso->validate(session('key'), session('secret'), $get->input('oauth_verifier'), function ($sso_data, $request) {
+                session()->forget('key');
+                session()->forget('secret');
+                User::updateOrCreate(['id' => $sso_data->id], ['fname' => $sso_data->name_first, 'lname' => $sso_data->name_last]);
+                $user = User::find($sso_data->id);
+                $subdivision = DivisionsController::getSubdivisionID($sso_data->region, $sso_data->division, $sso_data->subdivision);
+                $rating = RatingsController::getRatingID($sso_data->rating);
+                $pRatings = PRatingsController::getPRatingsIDs($sso_data->pilot_rating);
+                $user->subdivision()->sync($subdivision);
+                $user->rating()->sync($rating);
+                $user->p_ratings()->sync($pRatings);
+                
+                Auth::login($user, true);
+            });
+        } catch (SSOException $e) {
+            return redirect()->route('page')->withError($e->getMessage());
+        }
+        
+        return redirect()->intended(route('page'));
+    }
+    /**
+     * Log the user out
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function logout()
+    {
+        if (! Auth::check()) return redirect()->back();
+        Auth::logout();
+        return redirect()->route('page')->withSuccess('You have been successfully logged out.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,30 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//--------------------------------------------------------------------------
+// Main page
+//--------------------------------------------------------------------------
+Route::get('/', 'PageController@render')->name('page');
+//--------------------------------------------------------------------------
+// VATSIM Authentication
+//--------------------------------------------------------------------------
+Route::get('/login', 'Auth\LoginController@login')->middleware('guest')->name('login');
+Route::get('/validate', 'Auth\LoginController@validateLogin')->middleware('guest');
+Route::get('/logout', 'Auth\LoginController@logout')->middleware('auth')->name('logout');
+Route::middleware('auth')->group(function () {
+    //--------------------------------------------------------------------------
+    // Discord Authentication
+    //--------------------------------------------------------------------------
+    Route::get('/discord/login', 'DiscordAuthController@login')->name('discord.login');
+    Route::get('/discord/validate', 'DiscordAuthController@validateLogin');
+    //--------------------------------------------------------------------------
+    // Discord Role Management
+    //--------------------------------------------------------------------------
+    Route::put('guilds/{guild}/update', 'RoleController@saveRoleSettings')->name('role.update');
 });
 
+/*
 Auth::routes();
 
 Route::get('/home', 'DashboardController@index')->name('home');
+*/
