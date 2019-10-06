a48271e7b5a9225fbaa05993ba00bdfbd468cde2
blt950 <blt950@users.noreply.github.com>
2019-10-06 21:45:46 +0200

    Refactored front-end packages and dependecies

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,14 +55,7 @@
     </a>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('sbadmin2/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('sbadmin2/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('sbadmin2/jquery-easing/jquery.easing.min.js') }}"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('sbadmin2/js/sb-admin-2.min.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     @yield('js')
     </body>
 </html>
