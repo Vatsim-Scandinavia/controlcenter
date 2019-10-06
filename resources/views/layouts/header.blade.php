d6bf51d0e23cdca7c72accedb43bff8e54d6adcd
blt950 <blt950@users.noreply.github.com>
2019-10-06 21:46:46 +0200

    Added missing csrf token

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <title>@yield('title', 'Home') | {{ config('app.name') }}</title>
 
