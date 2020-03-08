80b6860bf26d7cd892de26f29576664d776661b7
blt950 <blt950@users.noreply.github.com>
2020-03-08 15:19:01 +0100

    Change to timestamps resolves #4

--- database/migrations/2020_03_08_000000_create_core_members_table.php
+++ database/migrations/2020_03_08_000000_create_core_members_table.php
@@ -23,14 +23,14 @@ class CreateCoreMembersTable extends Migration
             $table->string('ratingLong', 24);
             $table->string('ratingGRP', 32);
             $table->tinyInteger('pilotRating');
-            $table->dateTime('regDate');
+            $table->timestamp('regDate');
             $table->string('country', 2);
             $table->string('region', 8);
             $table->string('division', 3);
             $table->string('subdivision', 3);
             $table->tinyInteger('active');
             $table->tinyInteger('acceptedPrivacy');
-            $table->dateTime('lastLogin')->useCurrent();
+            $table->timestamp('lastLogin')->useCurrent();
         });
     }
 
