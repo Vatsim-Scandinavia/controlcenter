f923e1f1ad51a5e02bbca4e6af1879505a62d72e
Christian Kovanen <chr.kovanen@gmail.com>
2020-03-11 15:00:43 +0200

    fix

--- resources/views/vatbook/show.blade.php
+++ resources/views/vatbook/show.blade.php
@@ -53,8 +53,8 @@
                     @endif
 
                     <div class="form-group">
-                        <label for="mentor">Mentor</label>
-                        <input id="mentor" class="form-control" type="text" name="mentor" readonly="readonly" value="{{ $booking->user->handover->firstName }} {{ $booking->user->handover->lastName }} ({{ $booking->user->id }})">
+                        <label for="user">User</label>
+                        <input id="user" class="form-control" type="text" name="user" readonly="readonly" value="{{ $booking->user->handover->firstName }} {{ $booking->user->handover->lastName }} ({{ $booking->user->id }})">
                     </div>
 
                     <input type="hidden" name="id" value="{{{ $booking->id }}}"> 
