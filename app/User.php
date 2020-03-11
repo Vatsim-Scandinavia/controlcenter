b28266480b4aa4fd8b9f7f6a09c16a2deedc4edc
Christian Kovanen <chr.kovanen@gmail.com>
2020-03-11 03:34:06 +0200

    user relation to vatbook

--- app/User.php
+++ app/User.php
@@ -70,6 +70,10 @@ class User extends Authenticatable
         return $this->hasMany(Booking::class);
     }
 
+    public function vatbooks(){
+        return $this->hasMany(Vatbook::class);
+    }
+
     // User group checks
     public function isMentor(){
         return $this->group <= 3 && isset($this->group);
