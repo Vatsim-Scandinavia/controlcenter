9dd994cafd8352cd5fa3d56e309c7c9ea5349acc
blt950 <blt950@users.noreply.github.com>
2020-03-15 17:34:20 +0100

    Endorsements can now be created

--- resources/views/user/endorsement/overview.blade.php
+++ resources/views/user/endorsement/overview.blade.php
@@ -32,12 +32,11 @@
                             </tr>
                         </thead>
                         <tbody>
-                            
                             @foreach($endorsements as $endorsement)
                             <tr>
                                 <td>
                                     @if($user->isModerator())
-                                        <a href="/user/endorsement/edit/{{ $endorsement->id }}">{{ $endorsement->user->handover->firstName }} {{ $endorsement->user->handover->lastName }}</a>
+                                        <a href="/user/endorsement/edit/{{ $endorsement->user_id }}">{{ $endorsement->user->name() }} ({{ $endorsement->user->id }})</a>
                                     @else
                                         {{ $endorsement->user->handover->firstName }} {{ $endorsement->user->handover->lastName }}
                                     @endif
