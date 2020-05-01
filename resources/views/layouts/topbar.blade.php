db84465cfe3f98e2d8a30617bc16c0c547940f21
blt950 <blt950@users.noreply.github.com>
2020-05-01 20:57:09 +0200

    Disabled alerts center
    It'll be implemented on a later note, e-mail work fine until then.

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -45,36 +45,48 @@
         <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-bell fa-fw"></i>
             <!-- Counter - Alerts -->
-            <span class="badge badge-danger badge-counter">3+</span>
+            <!--<span class="badge badge-danger badge-counter">3+</span>-->
         </a>
         <!-- Dropdown - Alerts -->
         <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
             <h6 class="dropdown-header">
-            Alerts Center
+            Alerts
             </h6>
+
             <a class="dropdown-item d-flex align-items-center" href="#">
-            <div class="mr-3">
-                <div class="icon-circle bg-success">
-                <i class="fas fa-check text-white"></i>
+                <div class="mr-3">
+                    <div class="icon-circle bg-warning">
+                    <i class="fas fa-exclamation text-white"></i>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="small text-gray-500">December 12, 2019</div>
-                Your student Name Nameson has passed their S3 ATSimTest
-            </div>
+                <div>
+                    Alerts will be implemented later. Please check your e-mail for alerts.
+                </div>
+            </a>
+            <!--
+            <a class="dropdown-item d-flex align-items-center" href="#">
+                <div class="mr-3">
+                    <div class="icon-circle bg-success">
+                    <i class="fas fa-check text-white"></i>
+                    </div>
+                </div>
+                <div>
+                    <div class="small text-gray-500">December 12, 2019</div>
+                    Your student Name Nameson has passed their S3 ATSimTest
+                </div>
             </a>
             <a class="dropdown-item d-flex align-items-center" href="#">
-            <div class="mr-3">
-                <div class="icon-circle bg-warning">
-                <i class="fas fa-exclamation-triangle text-white"></i>
+                <div class="mr-3">
+                    <div class="icon-circle bg-warning">
+                    <i class="fas fa-exclamation-triangle text-white"></i>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="small text-gray-500">December 2, 2019</div>
-                Your student Name Nameson has not had training in over two months.
-            </div>
+                <div>
+                    <div class="small text-gray-500">December 2, 2019</div>
+                    Your student Name Nameson has not had training in over two months.
+                </div>
             </a>
-            <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+            <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>-->
         </div>
         </li>
 
