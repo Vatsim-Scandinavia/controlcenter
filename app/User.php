21911d2c801a0a2017f728f8a55074dd9eaa6751
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-05-31 21:03:21 +0200

    "Laravelify" User

--- app/User.php
+++ app/User.php
@@ -40,7 +40,8 @@ class User extends Authenticatable
      *
      * @return \App\Handover
      */
-    public function handover(){
+    public function handover()
+    {
         return $this->hasOne(Handover::class, 'id');
     }
 
@@ -49,35 +50,43 @@ class User extends Authenticatable
      *
      * @return \App\Solo
      */
-    public function soloEndorsement(){
+    public function soloEndorsement()
+    {
         return $this->hasOne(Solo::class);
     }
 
-    public function trainings(){
+    public function trainings()
+    {
         return $this->hasMany(Training::class);
     }
 
-    public function group(){
+    public function group()
+    {
         return $this->belongsTo(Group::class);
     }
 
-    public function teaches(){
+    public function teaches()
+    {
         return $this->belongsToMany(Training::class);
     }
 
-    public function ratings(){
+    public function ratings()
+    {
         return $this->belongsToMany(Rating::class);
     }
 
-    public function settings(){
+    public function settings()
+    {
         return $this->hasMany(UserSetting::class);
     }
 
-    public function bookings(){
+    public function bookings()
+    {
         return $this->hasMany(Booking::class);
     }
 
-    public function vatbooks(){
+    public function vatbooks()
+    {
         return $this->hasMany(Vatbook::class);
     }
 
@@ -87,27 +96,33 @@ class User extends Authenticatable
     }
 
     // Get properties from Handover, the variable names here break with the convention.
-    public function getLastNameAttribute(){
+    public function getLastNameAttribute()
+    {
         return $this->getHandoverAttr('last_name');
     }
 
-    public function getFirstNameAttribute(){
+    public function getFirstNameAttribute()
+    {
         return $this->getHandoverAttr('first_name');
     }
 
-    public function getNameAttribute(){
+    public function getNameAttribute()
+    {
         return $this->first_name . " " . $this->last_name;
     }
 
-    public function getRatingAttribute(){
+    public function getRatingAttribute()
+    {
         return $this->getHandoverAttr('rating');
     }
 
-    public function getRatingShortAttribute(){
+    public function getRatingShortAttribute()
+    {
         return $this->getHandoverAttr('rating_short');
     }
 
-    public function getRatingLongAttribute(){
+    public function getRatingLongAttribute()
+    {
         return $this->getHandoverAttr('rating_long');
     }
 
@@ -145,7 +160,7 @@ class User extends Authenticatable
             throw new PolicyMissingException();
         }
 
-        if ( ! method_exists(policy($class), 'view')) {
+        if (!method_exists(policy($class), 'view')) {
             throw new PolicyMethodMissingException('The view method does not exist on the policy.');
         }
 
@@ -162,23 +177,26 @@ class User extends Authenticatable
     }
 
     // User group checks
-    public function isMentor(Country $country = null) {
+    public function isMentor(Country $country = null)
+    {
 
         if ($country == null) {
             return $this->group <= 3 && isset($this->group);
         }
 
-        return  $this->group <= 3 &&
-                isset($this->group) &&
-                $country->mentors->contains($this);
+        return $this->group <= 3 &&
+            isset($this->group) &&
+            $country->mentors->contains($this);
 
     }
 
-    public function isModerator(){
+    public function isModerator()
+    {
         return $this->group <= 2 && isset($this->group);
     }
 
-    public function isAdmin(){
+    public function isAdmin()
+    {
         return $this->group <= 1 && isset($this->group);
     }
 }
