4c554f4ad0a2bbd77c180d3e470c4629a9ed713f
blt950 <blt950@users.noreply.github.com>
2020-06-03 20:27:19 +0200

    Fixed dashbaord and memberlist colors

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -93,18 +93,18 @@
     <div class="col-xl-8 col-lg-7">
     <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
-        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-        <h6 class="m-0 font-weight-bold text-primary">My Trainings</h6>
+        <div class="card-header bg-primary py-3 d-flex flex-row align-items-center justify-content-between">
+        <h6 class="m-0 font-weight-bold text-white">My Trainings</h6>
         </div>
         <!-- Card Body -->
-        <div class="card-body">
+        <div class="card-body p-0">
 
         @if (sizeof($trainings) == 0)
             You have no trainings.
         @else
         <div class="table-responsive">
-            <table class="table table-striped" width="100%" cellspacing="0">
-            <thead>
+            <table class="table table-striped table-hover table-leftpadded mb-0" width="100%" cellspacing="0">
+            <thead class="thead-light">
                 <tr>
                 <th>Level</th>
                 <th>Country</th>
@@ -174,8 +174,8 @@
     <div class="col-xl-4 col-lg-5">
     <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
-        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-        <h6 class="m-0 font-weight-bold text-primary">Training</h6>
+        <div class="card-header bg-primary py-3 d-flex flex-row align-items-center justify-content-between">
+        <h6 class="m-0 font-weight-bold text-white">Training</h6>
         </div>
         <!-- Card Body -->
         <div class="card-body">
