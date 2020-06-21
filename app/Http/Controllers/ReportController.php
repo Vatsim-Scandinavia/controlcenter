fb6197a1ce1cacfd9587d41a98bfa49387240925
blt950 <blt950@users.noreply.github.com>
2020-06-21 13:17:20 +0200

    Working 6m stats for all firs

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -53,19 +53,10 @@ class ReportController extends Controller
         $totalRequests = [];
         if($filterCountry){
 
-            $data = Training::select([
-                // This aggregates the data and makes available a 'count' attribute
-                DB::raw('count(id) as `count`'), 
-                // This throws away the timestamp portion of the date
-                DB::raw('DATE(created_at) as day')
-              // Group these records according to that day
-              ])->groupBy('day')
-              // Show only the filtered country
+            $data = Training::select([DB::raw('count(id) as `count`'), DB::raw('DATE(created_at) as day')])->groupBy('day')
               ->where('country_id', $filterCountry)
-              // And restrict these results to only those created in the last week
               ->where('created_at', '>=', Carbon::now()->subYear(1))
-              ->get()
-            ;
+              ->get();
 
             foreach($data as $entry) {
                 array_push($totalRequests, ['t' => $entry->day, 'y' => $entry->count]);
@@ -74,14 +65,10 @@ class ReportController extends Controller
         } else {
             $data = Training::select([
                 DB::raw('count(id) as `count`'), 
-                // This throws away the timestamp portion of the date
                 DB::raw('DATE(created_at) as day')
-              // Group these records according to that day
               ])->groupBy('day')
-              // And restrict these results to only those created in the last week
               ->where('created_at', '>=', Carbon::now()->subYear(1))
-              ->get()
-            ;
+              ->get();
 
             foreach($data as $entry) {
                 array_push($totalRequests, ['t' => $entry->day, 'y' => $entry->count]);
@@ -139,12 +126,47 @@ class ReportController extends Controller
                     foreach($query as $entry) {
                         $completedRequests[$rating->name][$monthTranslator[$entry->month]] = $entry->count;
                     }
-
-                    //dd($newRequests, $completedRequests);
                 }
             }
 
         } else {
+
+            foreach(Rating::all() as $rating){
+                if($rating->vatsim_rating && $rating->id >= 2 && $rating->id <= 4){
+
+                    $newRequests[$rating->name] = [0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0];
+                    $completedRequests[$rating->name] = [0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0];
+
+                    // New requests
+                    $query = DB::table('trainings')
+                        ->select(DB::raw('count(trainings.id) as `count`'), DB::raw('MONTH(trainings.created_at) as month'))
+                        ->join('rating_training', 'trainings.id', '=', 'rating_training.training_id')
+                        ->join('ratings', 'ratings.id', '=', 'rating_training.training_id')
+                        ->where('created_at', '>=', date("Y-m-d H:i:s", strtotime('-6 months')))
+                        ->where('rating_id', $rating->id)
+                        ->groupBy('month')
+                        ->get();
+
+                    foreach($query as $entry) {
+                        $newRequests[$rating->name][$monthTranslator[$entry->month]] = $entry->count;
+                    }
+
+                    // Completed requests
+                    $query = DB::table('trainings')
+                        ->select(DB::raw('count(trainings.id) as `count`'), DB::raw('MONTH(trainings.finished_at) as month'))
+                        ->join('rating_training', 'trainings.id', '=', 'rating_training.training_id')
+                        ->join('ratings', 'ratings.id', '=', 'rating_training.training_id')
+                        ->where('status', 3)
+                        ->where('finished_at', '>=', date("Y-m-d H:i:s", strtotime('-6 months')))
+                        ->where('rating_id', $rating->id)
+                        ->groupBy('month')
+                        ->get();
+
+                    foreach($query as $entry) {
+                        $completedRequests[$rating->name][$monthTranslator[$entry->month]] = $entry->count;
+                    }
+                }
+            }
 
         }
 
