f0d758962a9aeb32b0d280c18ed25943b65387eb
blt950 <blt950@users.noreply.github.com>
2020-07-13 19:41:33 +0200

    Added favicon closing #38

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,4 +13,15 @@
 <!-- Custom styles for this template-->
 <link href="{{ asset('css/app.css' ) }}" rel="stylesheet">
 <link href="{{ asset('css/vendor.css' ) }}" rel="stylesheet">
+
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+<link rel="manifest" href="/images/favicon/site.webmanifest">
+<link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#1a475f">
+<link rel="shortcut icon" href="/images/favicon/favicon.ico">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="msapplication-config" content="/images/favicon/browserconfig.xml">
+<meta name="theme-color" content="#1a475f">
 @yield('css')
