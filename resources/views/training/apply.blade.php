491c5f2d75ad3171a7dc2cd0e100cf9c6335a89f
blt950 <blt950@users.noreply.github.com>
2020-07-19 22:50:38 +0200

    Tweaked layout of apply and added queue info

--- resources/views/training/apply.blade.php
+++ resources/views/training/apply.blade.php
@@ -9,7 +9,7 @@
 
     @if(!Request::get('step'))
     <!-- Information about training -->
-    <div class="col-xl-12 col-md-12 mb-12">
+    <div class="col-xl-6 col-lg-12 col-md-12 mb-12">
         <div class="card shadow mb-4 border-left-danger">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Important information</h6>
@@ -17,17 +17,20 @@
             <div class="card-body">
                 <h5 class="card-title">What is ATC trainig?</h5>
                 <p class="card-text">Welcome to the ATC Training Department of VATSIM Scandinavia. In order to be able to control on our network you will need to complete our training course. To achieve an ATC rating you have to go through both theoretical and practical training and exams. You will be given all the necessary training documentation and will receive guidance by a mentor throughout the course. You will learn everything you need to know to be compliant with VATSIM Global Ratings Policy as well as about local procedures relevant to your area.</p>
-
+                <hr>
                 <h5 class="card-title">What do we expect from you?</h5>
                 <p class="card-text">First of all, we expect that you take the training seriously and for you to show up on time and prepared for your online training sessions. We also expect that you respect that everyone in the Training Department is doing this as a hobby in their spare time. You have to be able to study on your own as part of the training program is designed as a self-study. We are not getting paid to do this job, but we simply want to see our network grow and be a great community.</p>
-
+                <hr>
                 <h5 class="card-title">What should you expect from us?</h5>
                 <p class="card-text">You should expect that we will help you as best as we can to prepare you for your practical exam. You will be assigned to a mentor that will guide you on the way, and you should expect him to take you and your time seriously and to adapt the training to your level of competence.</p>
+                <hr>
+                <h5 class="card-title">How long is the training queue?</h5>
+                <p class="card-text">{{ Setting::get('trainingQueue') }}</p>
             </div>
         </div>
     </div>
 
-    <div class="col-xl-12 col-md-12 mb-12">
+    <div class="col-xl-6 col-lg-12 col-md-12 mb-12">
 
         <div class="card shadow mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
