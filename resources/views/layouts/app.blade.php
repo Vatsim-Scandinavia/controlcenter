b4a63699631549d1c4b6de8328daeade4561b4d5
blt950 <blt950@users.noreply.github.com>
2020-08-26 18:37:16 +0200

    Moved hotjar script to head as required

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -146,20 +146,6 @@
         });
     </script>
 
-    @if(!empty(Config::get('app.hotjar')))
-    <!-- Hotjar Tracking Code -->
-    <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:1961036,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
-    @endif
-
     @yield('js')
     </body>
 </html>
