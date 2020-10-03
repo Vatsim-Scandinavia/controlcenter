5792e81579fc1ac1cbbc1dce57316cf36d129c0b
blt950 <blt950@users.noreply.github.com>
2020-10-03 23:05:30 +0200

    Disabled update:members
    We need to get better Handover data before we run this

--- app/Console/Commands/UpdateMemberDetails.php
+++ app/Console/Commands/UpdateMemberDetails.php
@@ -66,7 +66,7 @@ class UpdateMemberDetails extends Command
         // Get active trainings
         $trainings = Training::where('status', '>=', 0)->where('type', '!=', 5)->get();
 
-        $this->info("Closing trainings for those who left subdivision...");
+        $this->info("Closing trainings for those who left subdivision: ".$trainings->count()." members.");
 
         foreach ($trainings as $training) {
 
