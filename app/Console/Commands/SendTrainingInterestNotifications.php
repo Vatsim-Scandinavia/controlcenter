21b139d8ca221939acebc12ff1b141896230ef78
blt950 <blt950@users.noreply.github.com>
2020-10-03 18:58:21 +0200

    Fixed training interest email reminders

--- app/Console/Commands/SendTrainingInterestNotifications.php
+++ app/Console/Commands/SendTrainingInterestNotifications.php
@@ -47,7 +47,7 @@ class SendTrainingInterestNotifications extends Command
     public function handle()
     {
 
-        $trainings = Training::where([['started_at', null], ['created_at', '<=', now()->subtract('d', 30)], ['status', '=', 0]])->get();
+        $trainings = Training::where([['started_at', null], ['created_at', '<=', Carbon::now()->subDays(30)], ['status', '=', 0]])->get();
 
         foreach ($trainings as $training) {
 
@@ -55,40 +55,44 @@ class SendTrainingInterestNotifications extends Command
 
             if($lastInterestRequest == null) {
                 // A notification has NOT been sent previously
-                // Check if training in question is at least 30 days old
-                if ($training->created_at->diffInDays(now()) >= 30) {
+                // Generate training interest key and store it in the request
+                $key = sha1($training->id . now()->format('Ymd_His') . rand(0, 9999));
 
-                    // Generate training interest key and store it in the request
-                    $key = sha1($training->id . now()->format('Ymd_His') . rand(0, 9999));
+                $interest = TrainingInterest::create([
+                    'training_id' => $training->id,
+                    'key' => $key,
+                    'deadline' => now()->addDays(14),
+                ]);
 
-                    $interest = TrainingInterest::create([
-                        'training_id' => $training->id,
-                        'key' => $key,
-                        'deadline' => now()->addDays(14),
-                    ]);
-
-                    // Send notification to student
-                    $training->user->notify(new TrainingInterestNotification($training, $interest));
-
-                }
+                // Send notification to student
+                $training->user->notify(new TrainingInterestNotification($training, $interest));
+                
             } else {
                 // A notification has been sent previously
 
                 $requestDeadline = $lastInterestRequest->deadline;
                 $requestConfirmed = $lastInterestRequest->confirmed_at;
-                
+                $requestUpdated = $lastInterestRequest->updated_at;
+
                 if ($requestDeadline->diffInDays(now()) <= 0 && $requestConfirmed == null) {
                     // If it's 14 days passed deadline, close the training
+                    $this->info("Closing training ".$training->id);
+
                     $training->updateStatus(-4);
                     $training->user->notify(new TrainingClosedNotification($training, -4, 'Continued training interested was not confirmed within deadline.'));
 
 
-                } elseif ($requestDeadline->diffInDays(now()) <= 7 && $requestConfirmed == null) {
+                } elseif ($requestDeadline->diffInDays(now()) == 7 && $requestUpdated->diffInDays(now()) != 0 && $requestConfirmed == null) {
                     // If the interest is not confirmed after 7 days, we remind
+                    $this->info("Reminding training ".$training->id);
+
+                    $requestUpdated = now();
+                    $lastInterestRequest->save();
+
                     $training->user->notify(new TrainingInterestNotification($training, $lastInterestRequest, true));     
 
 
-                } elseif ($lastInterestRequest->created_at->diffInDays(now()) >= 30 && $training->created_at->diffInDays(now()) >= 30) {
+                } elseif ($lastInterestRequest->created_at->diffInDays(now()) >= 30) {
                     // The training has been previously notified, after 30 days it's time for a new request
                     // Generate training interest key and store it in the request
                     $key = sha1($training->id . now()->format('Ymd_His') . rand(0, 9999));
