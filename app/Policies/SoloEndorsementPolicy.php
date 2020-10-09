3fe31e996b805a6c433360d5a22a096f645a7c72
Christian Kovanen <chr.kovanen@gmail.com>
2020-10-09 05:32:42 +0300

    Move checks to policy #165

---
+++ app/Policies/SoloEndorsementPolicy.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class SoloEndorsementPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view bookings.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function view(User $user)
+    {
+        return $user->isMentor();
+    }
+
+    /**
+     * Determine whether the user can create bookings.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->isModerator();
+    }
+
+    /**
+     * Determine whether the user can update the booking.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function update(User $user)
+    {
+        return $user->isModerator();
+    }
+}
