65b2e98e6dacca0c349073a727313fb218493302
blt950 <blt950@users.noreply.github.com>
2020-10-18 11:00:36 +0200

    Fixed vat+sweatbook sorting

--- resources/views/vatbook/index.blade.php
+++ resources/views/vatbook/index.blade.php
@@ -25,10 +25,10 @@
                         data-page-list=[10,15,25,50]>
                         <thead class="thead-light">
                             <tr>
-                                <th data-field="date" data-sortable="true" data-sorter="tableSortDates" data-filter-control="select" data-filter-data-collector="tableFilterStripHtml">Date</th>
+                                <th data-field="date" data-sortable="true" data-sorter="tableSortDates" data-filter-data-collector="tableFilterStripHtml">Date</th>
                                 <th data-field="start" data-sortable="true" data-filter-control="select">Start (Zulu)</th>
                                 <th data-field="end" data-sortable="true" data-filter-control="select">End (Zulu)</th>
-                                <th data-field="position" data-sortable="true" data-filter-control="select">Position</th>
+                                <th data-field="position" data-sortable="true" data-filter-control="select" data-filter-data-collector="tableFilterStripHtml">Position</th>
                                 <th data-field="fir" data-sortable="true" data-filter-control="select">FIR</th>
                                 <th data-field="user" data-sortable="true" data-filter-control="select">User</th>
                             </tr>
