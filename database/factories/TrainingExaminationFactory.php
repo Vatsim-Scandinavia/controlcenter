7ee880dfb36002b9565fc55644126c21aafc9782
blt950 <blt950@users.noreply.github.com>
2020-11-02 20:13:16 +0100

    Finished seeds and factories #143

--- database/factories/TrainingExaminationFactory.php
+++ database/factories/TrainingExaminationFactory.php
@@ -6,11 +6,16 @@ use App\TrainingExamination;
 use Faker\Generator as Faker;
 
 $factory->define(TrainingExamination::class, function (Faker $faker) {
+
+    $date = $faker->dateTimeBetween($startDate = 'now', $endDate = '+ 1 years');
+
     return [
-        'examination_date' => now()->addYears(5),
+        'examination_date' => $date,
         'position_id' => \App\Position::query()->inRandomOrder()->first()->id,
         'result' => $faker->randomElement([
             'PASSED', 'FAILED', 'INCOMPLETE', 'POSTPONED',
         ]),
+        'created_at' => $date,
+        'updated_at' => $date,
     ];
 });
