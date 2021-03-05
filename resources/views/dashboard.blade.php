29b74c56c8472fd788b43c17f7cc859a6f4f9055
blt950 <blt950@users.noreply.github.com>
2021-03-05 21:57:10 +0100

    Tweaked ATC hour texts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -63,12 +63,12 @@
 
     <!-- ATC Hours card -->
     <div class="col-xl-3 col-md-6 mb-4 d-none d-xl-block d-lg-block d-md-block">
-    <div class="card border-left-success shadow h-100 py-2">
+    <div class="card {{ ($atcHours < Setting::get('atcActivityRequirement', 10)) ? 'border-left-danger' : 'border-left-success' }} shadow h-100 py-2">
         <div class="card-body">
         <div class="row no-gutters align-items-center">
             <div class="col mr-2">
             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">ATC Hours (Last 12 months)</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $atcHours }}</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $atcHours ? $atcHours.' hours of 10 required' : 'N/A' }}</div>
             </div>
             <div class="col-auto">
             <i class="fas fa-clock fa-2x text-gray-300"></i>
