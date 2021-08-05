29dff01722cce795353c33f696a75e8bf3d0b4e5
blt950 <blt950@users.noreply.github.com>
2021-08-05 18:01:41 +0200

    Made vatbook table cookies save 2 weeks

--- resources/views/vatbook/index.blade.php
+++ resources/views/vatbook/index.blade.php
@@ -19,6 +19,7 @@
                     <table class="table table-striped table-sm table-hover table-leftpadded mb-0" width="100%" cellspacing="0"
                         data-cookie="true"
                         data-cookie-id-table="vatbook"
+                        data-cookie-expire="14d"
                         data-toggle="table"
                         data-pagination="true"
                         data-strict-search="true"
