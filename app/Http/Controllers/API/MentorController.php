dc7e854ae283b86089018f405d679c6ebbc54355
blt950 <blt950@users.noreply.github.com>
2022-01-02 16:55:25 +0100

    Fixed mentor API FIRs

--- app/Http/Controllers/API/MentorController.php
+++ app/Http/Controllers/API/MentorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Area;
 use Illuminate\Http\Request;
 
 class MentorController extends Controller
@@ -15,9 +16,18 @@ class MentorController extends Controller
 
         foreach ($users as $user) {
             if ($user->isMentor()) {
+
+                // Get their areas
+                $areas = [];
+                foreach($user->groups as $group){
+                    if($group->pivot->group_id == 3){
+                        array_push($areas, Area::find($group->pivot->area_id)->name);
+                    }
+                }
+
                 $mentors->push([
                     'id' => $user->id,
-                    'fir' => $user->getInlineMentoringAreas()
+                    'fir' => $areas
                 ]);
             }
         }
