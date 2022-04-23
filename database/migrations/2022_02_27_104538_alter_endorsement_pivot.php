a028a800aea1f57339795355776114c53b98bf7a
blt950 <blt950@users.noreply.github.com>
2022-04-23 17:02:50 +0200

    Added MAE convertion on upgrade

--- database/migrations/2022_02_27_104538_alter_endorsement_pivot.php
+++ database/migrations/2022_02_27_104538_alter_endorsement_pivot.php
@@ -39,6 +39,32 @@ class AlterEndorsementPivot extends Migration
             $table->foreign('area_id')->references('id')->on('areas')->onDelete('cascade');
             $table->foreign('endorsement_id')->references('id')->on('endorsements')->onDelete('cascade');
         });
+
+        // Re-create the MAE endorsements from pre-upgrade
+        $trainings = \App\Models\Training::where('status', -1)->get();
+        
+        foreach($trainings as $training){
+
+            if(\App\Models\TrainingExamination::where('result', '=', 'PASSED')->where('training_id', $training->id)->exists()){
+
+                foreach($training->ratings as $rating){
+                    if($rating->vatsim_rating == null){
+
+                        $endorsement = new \App\Models\Endorsement();
+                        $endorsement->user_id = $training->user->id;
+                        $endorsement->type = "MASC";
+                        $endorsement->valid_from = $training->closed_at->format('Y-m-d H:i:s');
+                        $endorsement->valid_to = null;
+                        $endorsement->issued_by = null;
+                        $endorsement->save();
+
+                        $endorsement->ratings()->save(\App\Models\Rating::find($rating->id));
+                        
+                    }
+                }
+            }
+
+        }
     }
 
     /**
