f7f889d7edab97cdffbf2447c6377373a929da79
blt950 <blt950@users.noreply.github.com>
2022-05-07 23:29:51 +0200

    Updated to Laravel 9

--- config/database.php
+++ config/database.php
@@ -94,7 +94,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'schema' => 'public',
+            'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
 
