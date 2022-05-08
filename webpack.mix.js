2e3c924773fb6647814ece4e7fa1e0be38fe2a73
blt950 <blt950@users.noreply.github.com>
2022-05-08 14:17:28 +0200

    Added color and border theming #411

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,5 +19,7 @@ mix.js([
     'node_modules/bootstrap-table/dist/extensions/cookie/bootstrap-table-cookie.js',
     'node_modules/moment/moment.js',
 ], 'public/js').vue()
-.sass('resources/sass/app.scss', 'public/css')
+.sass('resources/sass/app.scss', 'public/css', {
+    additionalData: '$envColorPrimary: ' + (process.env.BOOTSTRAP_COLOR_PRIMARY || '#1a475f') + '; $envColorSecondary: ' + (process.env.BOOTSTRAP_COLOR_SECONDARY || '#484b4c') + '; $envColorTertiary: ' + (process.env.BOOTSTRAP_COLOR_TERTIARY || '#011328') + '; $envColorInfo: ' + (process.env.BOOTSTRAP_COLOR_INFO || '#17a2b8') + '; $envColorSuccess: ' + (process.env.BOOTSTRAP_COLOR_SUCCESS || '#41826e') + '; $envColorWarning: ' + (process.env.BOOTSTRAP_COLOR_WARNING || '#ff9800') + '; $envColorDanger: ' + (process.env.BOOTSTRAP_COLOR_DANGER || '#b63f3f') + '; $envBorderRadius: ' + (process.env.BOOTSTRAP_BORDER_RADIUS || '0px') + ';',
+})
 .sass('resources/sass/vendor.scss', 'public/css')
