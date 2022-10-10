21c91c18274e63658a00c44fd620c6c546d7ce17
blt950 <blt950@users.noreply.github.com>
2022-10-10 22:15:35 +0200

    Fixed mime error in attachments

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -27,7 +27,7 @@ class FileController extends Controller
         $this->authorize('view', $file);
 
         if (Storage::exists($file->full_path)) {
-            return response()->file(Storage::path($file->full_path));
+            return response()->file(Storage::path($file->full_path), ['Content-Disposition' => 'inline; filename="'.$file->name.'"', 'Content-Type' => Storage::mimeType($file->name)]);
         } else {
             return abort(404);
         }
