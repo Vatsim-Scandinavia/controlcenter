3dad3b57759143a8b67a02b4ac5642a874476139
Daniel <blt950@users.noreply.github.com>
2022-12-21 19:38:08 +0100

    Catch position errors on bulk bookings

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -226,7 +226,14 @@ class BookingController extends Controller
             $booking->time_end = Carbon::createFromFormat('H:i', $data['end_at'])->setDateFrom($date);
 
             $booking->callsign = strtoupper($position);
-            $booking->position_id = Position::all()->firstWhere('callsign', strtoupper($position))->id;
+
+            $positionModel = Position::all()->firstWhere('callsign', strtoupper($position));
+            if(isset($positionModel)){
+                $booking->position_id = Position::all()->firstWhere('callsign', strtoupper($position))->id;
+            } else {
+                return redirect(route('booking'))->withErrors('The position '.$position.' does not exist. The bulk booking stopped here, but previous positions in the list have been booked.')->withInput();
+            }
+            
             $booking->name = $user->name;
             $booking->user_id = $user->id;
 
