4564c84b50e5a7e258a3899398c562aa6308bcac
Daniel <blt950@users.noreply.github.com>
2022-12-30 15:06:06 +0100

    VATSIM rating helper
    Co-Authored-By: Thor K. Høgås <thor@roht.no>
    Co-Authored-By: meltinglava <17087998+meltinglava@users.noreply.github.com>

---
+++ app/Helpers/Vatsim.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Constants for VATSIM ratings.
+ */
+enum VatsimRating: int {
+    case S1 = 2;
+    case S2 = 3;
+    case S3 = 4;
+    case C1 = 5;
+    case C3 = 7;
+    case I1 = 8;
+    case I3 = 10;
+    case SUPERVISOR = 11;
+}
+?>
