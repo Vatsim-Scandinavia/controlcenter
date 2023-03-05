c5a6f267a50e813d0bbd81cc5e24b2ebc92901d9
Daniel <blt950@users.noreply.github.com>
2023-03-05 10:20:28 +0100

    Fixed Vatsim helper and ATC hours error

--- app/Helpers/Vatsim.php
+++ app/Helpers/Vatsim.php
@@ -1,29 +1,33 @@
 <?php
 
-namespace App\Helpers\Vatsim;
-
+namespace App\Helpers;
 use Illuminate\Support\Collection;
 
-/**
-* Checks if a position belongs to the division.
-* @param string callsign
-* @param Collection<string> divisionCallsignPrefixes
-*/
-function isDivisionCallsign(string $callsign, Collection $divisionCallsignPrefixes)
-{
-    $validAtcSuffixes = ["DEL" => true, "GND" => true, "TWR" => true, "APP" => true, "DEP" => true, "CTR" => true, "FSS" => true];
-    // Filter away invalid ATC suffixes
-    $suffix = substr($callsign, -3);
-    if (!array_key_exists($suffix, $validAtcSuffixes)) {
+class Vatsim{
+
+    /**
+    * Checks if a position belongs to the division.
+    * @param string callsign
+    * @param Collection<string> divisionCallsignPrefixes
+    */
+    public static function isDivisionCallsign(string $callsign, Collection $divisionCallsignPrefixes)
+    {
+        $validAtcSuffixes = ["DEL" => true, "GND" => true, "TWR" => true, "APP" => true, "DEP" => true, "CTR" => true, "FSS" => true];
+        // Filter away invalid ATC suffixes
+        $suffix = substr($callsign, -3);
+        if (!array_key_exists($suffix, $validAtcSuffixes)) {
+            return false;
+        }
+
+        // PREFIX
+        if ($divisionCallsignPrefixes->contains(substr($callsign, 0, 4))) {
+            return true;
+        }
+
         return false;
     }
 
-    // PREFIX
-    if ($divisionCallsignPrefixes->contains(substr($callsign, 0, 4))) {
-        return true;
-    }
-
-    return false;
 }
+
 
 ?>
