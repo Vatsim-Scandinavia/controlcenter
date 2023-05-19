dc9cec31bb6f9aa5766e30a5f6ee6dde67a7b312
Daniel <blt950@users.noreply.github.com>
2023-05-19 12:43:30 +0200

    Fix: Duplicate import

--- database/migrations/2022_12_30_135008_alter_atcativity_table.php
+++ database/migrations/2022_12_30_135008_alter_atcativity_table.php
@@ -3,7 +3,6 @@
 use anlutro\LaravelSettings\Facade as Setting;
 use anlutro\LaravelSettings\Facade as Setting;
 use App\Models\AtcActivity;
-use App\Models\AtcActivity;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
