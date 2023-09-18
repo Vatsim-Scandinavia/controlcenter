ebc8dad916fc8ed153d09fb59ed6f0e14ba64f94
Daniel <blt950@users.noreply.github.com>
2023-09-18 21:46:56 +0200

    Fixed bootstrap-table dep.

--- resources/js/bootstrap-table.js
+++ resources/js/bootstrap-table.js
@@ -3,3 +3,7 @@
 */
 
 import bootstrapTable from 'bootstrap-table';
+
+import bootstrapTableEnUS from 'bootstrap-table/dist/locale/bootstrap-table-en-US.js';
+import bootstrapTableFilterControl from 'bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.js';
+import bootstrapTableCookie from 'bootstrap-table/dist/extensions/cookie/bootstrap-table-cookie.js';
