d147d7194292cf41cec6b5120b580bc660cd815e
Thor K. Høgås <thor@roht.no>
2023-09-20 20:37:50 +0200

    misc: add auto-refresh for development

--- vite.config.js
+++ vite.config.js
@@ -28,18 +28,21 @@ export default ({ mode }) => {
     return defineConfig({
         plugins: [
             vue(),
-            laravel([
-                '/resources/sass/app.scss',
-                '/resources/js/app.js',
-                '/resources/js/vue.js',
-                '/resources/js/easymde.js',
-                '/resources/sass/easymde.scss',
-                '/resources/js/chart.js',
-                '/resources/js/flatpickr.js',
-                '/resources/sass/flatpickr.scss',
-                '/resources/js/bootstrap-table.js',
-                '/resources/sass/bootstrap-table.scss',
-            ]),
+            laravel({
+                input: [
+                    "/resources/sass/app.scss",
+                    "/resources/js/app.js",
+                    "/resources/js/vue.js",
+                    "/resources/js/easymde.js",
+                    "/resources/sass/easymde.scss",
+                    "/resources/js/chart.js",
+                    "/resources/js/flatpickr.js",
+                    "/resources/sass/flatpickr.scss",
+                    "/resources/js/bootstrap-table.js",
+                    "/resources/sass/bootstrap-table.scss",
+                ],
+                refresh: true,
+            }),
         ],
         resolve: {
             alias: {
