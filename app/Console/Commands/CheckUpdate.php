d00f3e8d9db103ac3cf8075a3a645291d9526258
Daniel <blt950@users.noreply.github.com>
2023-10-04 09:19:57 +0200

    Pint, cheers :beers:

--- app/Console/Commands/CheckUpdate.php
+++ app/Console/Commands/CheckUpdate.php
@@ -33,17 +33,17 @@ class CheckUpdate extends Command
         $currentVersion = 'v' . config('app.version');
         $releasedData = Http::get('https://api.github.com/repos/Vatsim-Scandinavia/controlcenter/releases')->json();
 
-        if(isset($releasedData) && isset($releasedData[0]) && isset($releasedData[0]['name'])){
+        if (isset($releasedData) && isset($releasedData[0]) && isset($releasedData[0]['name'])) {
             $releasedVersion = $releasedData[0]['name'];
 
             if ($currentVersion != $releasedVersion) {
                 $this->info("There's a new version of Control Center available! Please update to $releasedVersion.");
                 Setting::set('_updateAvailable', $releasedVersion);
             } else {
-                $this->info("Control Center is up to date.");
+                $this->info('Control Center is up to date.');
                 Setting::forget('_updateAvailable');
             }
-    
+
             Setting::save();
 
         }
