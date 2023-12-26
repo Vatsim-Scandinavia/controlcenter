Vatsim-Scandinavia/controlcenter

commit 4a3e10b74d0b31249e10d77b8dbf8cb1ff6ae671

feat: quick add for tasks
Added quick insert buttons for tasks. This also makes it a lot easier to use mobile as it won't display the names only Ids in autocomplete.

resources/views/training/parts/taskmodal.blade.php
@@ -21,24 +21,38 @@
 
                     <div class="mt-3">
                         <label class="form-label" for="user">Send request to</label>
-                        <input 
-                            id="user"
-                            class="form-control"
-                            type="text"
-                            name="assignee_user_id"
-                            list="userList"
-                            autocomplete="off"
-                            required
-                        >
-                        <datalist id="userList">
-                            @foreach(\App\Models\User::has('groups')->get() as $user)
-                                <option value="{{ $user->id }}">{{ $user->name }}</option>
-                            @endforeach
-                        </datalist>
+                        <div class="mt-1">
+                            <input 
+                                id="{{ Str::camel($requestType->getName()) }}User"
+                                class="form-control"
+                                type="text"
+                                name="assignee_user_id"
+                                list="userList"
+                                autocomplete="off"
+                                placeholder=""
+                                required
+                            >
+                            <datalist id="{{ Str::camel($requestType->getName()) }}UserList">
+                                @foreach(\App\Models\User::has('groups')->get() as $user)
+                                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                @endforeach
+                            </datalist>
 
-                        <input type="hidden" name="type" value="{{ $requestType::class }}">
-                        <input type="hidden" name="subject_user_id" value="{{ $training->user->id }}">
-                        <input type="hidden" name="subject_training_id" value="{{ $training->id }}">
+                            <div>
+                                @foreach($requestPopularAssignees as $user)
+                                    <button type="button" class="btn btn-sm btn-outline-primary mt-1" onclick="document.getElementById('{{ Str::camel($requestType->getName()) }}User').value = '{{ $user->id }}'">
+                                        <i class="fas fa-bolt"></i>
+                                        {{ $user->name }}
+                                    </button>
+                                @endforeach
+                            </div>
+
+                            <div class="mt-3">
+                                <input type="hidden" name="type" value="{{ $requestType::class }}">
+                                <input type="hidden" name="subject_user_id" value="{{ $training->user->id }}">
+                                <input type="hidden" name="subject_training_id" value="{{ $training->id }}">
+                            </div>
+                        </div>
                     </div>
 
                     @if($requestType->requireCheckboxConfirmation() !== false)

app/Http/Controllers/TaskController.php
@@ -139,6 +139,23 @@ class TaskController extends Controller
     }
 
     /**
+     * Get popular task Assignees
+     *
+     * @return Illuminate\Database\Eloquent\Collection;
+     */
+    public static function getPopularAssignees()
+    {
+        $users = User::has('tasks')->withCount('tasks')->orderBy('tasks_count', 'desc')->limit(10)->get();
+
+        // Filter out users who no longer can receive tasks and end up with 3
+        $users = $users->filter(function ($user) {
+            return $user->can('receive', Task::class);
+        })->take(3);
+
+        return $users;
+    }
+
+    /**
      * Check if a task type is valid
      *
      * @param  string  $type

app/Http/Controllers/TrainingController.php
@@ -344,8 +344,9 @@ class TrainingController extends Controller
         $activeTrainingInterest = TrainingInterest::where('training_id', $training->id)->where('expired', false)->get()->count();
 
         $requestTypes = TaskController::getTypes();
+        $requestPopularAssignees = TaskController::getPopularAssignees();
 
-        return view('training.show', compact('training', 'reportsAndExams', 'trainingMentors', 'statuses', 'types', 'experiences', 'activities', 'trainingInterests', 'activeTrainingInterest', 'requestTypes'));
+        return view('training.show', compact('training', 'reportsAndExams', 'trainingMentors', 'statuses', 'types', 'experiences', 'activities', 'trainingInterests', 'activeTrainingInterest', 'requestTypes', 'requestPopularAssignees'));
     }
 
     /**

app/Http/Controllers/TrainingExaminationController.php
@@ -37,8 +37,9 @@ class TrainingExaminationController extends Controller
 
         $positions = Position::all();
         $taskRecipients = collect(Group::admins()->merge(Group::moderators()));
+        $taskPopularAssignees = TaskController::getPopularAssignees();
 
-        return view('training.exam.create', compact('training', 'positions', 'taskRecipients'));
+        return view('training.exam.create', compact('training', 'positions', 'taskRecipients', 'taskPopularAssignees'));
     }
 
     /**

resources/views/training/exam/create.blade.php
@@ -113,7 +113,7 @@
                             <div class="border-top mb-4"></div>
 
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-12">
                                     <label class="form-label" for="user">Request rating upgrade from</label>
                                     <input 
                                         id="user"
@@ -128,6 +128,14 @@
                                             <option value="{{ $user->id }}">{{ $user->name }}</option>
                                         @endforeach
                                     </datalist>
+                                    <div>
+                                        @foreach($taskPopularAssignees as $user)
+                                            <button type="button" class="btn btn-sm btn-outline-primary mt-1 me-1" onclick="document.getElementById('user').value = '{{ $user->id }}'">
+                                                <i class="fas fa-bolt"></i>
+                                                {{ $user->name }}
+                                            </button>
+                                        @endforeach
+                                    </div>
                                     <small class="form-text">Training director for ratings or local staff for endorsements</small>
                                     @error('request_task_user_id')
                                         <span class="text-danger">{{ $errors->first('request_task_user_id') }}</span>

commit b84e22e0dfd4c3eef3c5178b94d9c695c7102f7d

fix: task datalist autocomplete bug introduced in 4a3e10b

resources/views/training/parts/taskmodal.blade.php
@@ -27,7 +27,7 @@
                                 class="form-control"
                                 type="text"
                                 name="assignee_user_id"
-                                list="userList"
+                                list="{{ Str::camel($requestType->getName()) }}UserList"
                                 autocomplete="off"
                                 placeholder=""
                                 required