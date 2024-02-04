6c817b0ae43918981a6ab1b1daa133417cc3ea10
Daniel <blt950@users.noreply.github.com>
2024-02-04 15:15:20 +0100

    fix: missing rename in solo index

--- resources/views/endorsements/solos.blade.php
+++ resources/views/endorsements/solos.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Training Endorsements')
+@section('title', 'Solo Endorsements')
 @section('title-flex')
     <div>
         @if (\Auth::user()->isModeratorOrAbove())
@@ -20,7 +20,7 @@
 
         <div class="card shadow mb-4">
             <div class="card-header bg-primary py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 fw-bold text-white">Training Endorsements</h6> 
+                <h6 class="m-0 fw-bold text-white">Solo Endorsements</h6> 
             </div>        
             <div class="card-body p-0">
                 <div class="table-responsive">
