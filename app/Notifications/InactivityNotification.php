98f5d064ca1798aff421d5a31457823ceb4d9741
Daniel <blt950@users.noreply.github.com>
2024-02-14 18:14:48 +0000

    typo

--- app/Notifications/InactivityNotification.php
+++ app/Notifications/InactivityNotification.php
@@ -57,7 +57,7 @@ class InactivityNotification extends Notification implements ShouldQueue
         }
 
         if (Setting::get('atcActivityAllowInactiveControlling')) {
-            $loginRuleString = 'You may however still log on the network in ' . (isset($this->area) ? $this->area->name : 'our area') . ' if you wish. Please check local policies for more information what it means to be inactive.';
+            $loginRuleString = 'You may however still log on the network in ' . (isset($this->area) ? $this->area->name : 'our area') . ' if you wish. Please check the local policies for more information what it means to be inactive.';
         } else {
             $loginRuleString = 'You are no longer allowed to log on the network in ' . (isset($this->area) ? $this->area->name : 'our area') . '. To control online again, you will need to apply for a refresh training with [' . Setting::get('atcActivityContact') . '](' . Setting::get('linkContact') . ')';
         }
