438346020daa617d2d0c672d15ddc5159c1ea9cd
Daniel <blt950@users.noreply.github.com>
2024-02-18 18:12:50 +0100

    Update DivisionApiContract.php

--- app/Contracts/DivisionApiContract.php
+++ app/Contracts/DivisionApiContract.php
@@ -8,6 +8,7 @@ use App\Models\User;
 interface DivisionApiContract
 {
     public function assignMentor(Area $area, User $user, int $requesterId);
+    public function removeMentor(Area $area, User $user, int $requesterId);
 
     public function assignTheoryExam($parameters);
 }
