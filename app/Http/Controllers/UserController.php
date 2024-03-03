891c81c951042a42fe0302fa1529bed6789c5dba
Daniel <blt950@users.noreply.github.com>
2024-03-03 16:34:03 +0100

    fix: improved sort of endorsements in user view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -112,7 +112,7 @@ class UserController extends Controller
         $trainings = $user->trainings;
         $statuses = TrainingController::$statuses;
         $types = TrainingController::$types;
-        $endorsements = $user->endorsements->whereIn('type', ['EXAMINER', 'MASC', 'SOLO', 'VISITING'])->sortByDesc('valid_to');
+        $endorsements = $user->endorsements->whereIn('type', ['EXAMINER', 'MASC', 'SOLO', 'VISITING'])->sortBy([['expired', 'asc'], ['revoked', 'asc']]);
 
         // Get hours and grace per area
         $atcActivityHours = [];
