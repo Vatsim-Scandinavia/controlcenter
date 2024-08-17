9b5de01e782a1885f819c295dd9a3956fee05468
amosngSP <40349656+amosngSP@users.noreply.github.com>
2024-08-18 01:31:05 +0800

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,6 +68,14 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
         ],
+        'vultr' => [
+            'driver' => 's3',
+            'key' => env('VULTR_ACCESS_KEY'),
+            'secret' => env('VULTR_SECRET_KEY'),
+            'region' => env('VULTR_REGION'),
+            'bucket' => env('VULTR_BUCKET'),
+            'endpoint' => env('VULTR_ENDPOINT'),
+        ],
 
     ],
 
