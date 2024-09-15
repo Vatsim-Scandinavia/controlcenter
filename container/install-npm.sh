a6ee92f77b0bf89c2642f0d405c96a7d46c0c413
Daniel <blt950@users.noreply.github.com>
2024-09-15 09:02:15 +0200

    dependency tweaks

---
+++ container/install-npm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Starting theme building process..."
+
+# Install
+apt update
+apt install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=22
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+
+apt update
+apt install nodejs -y
+
+# Build config
+npm config set cache /tmp --global
+
+echo "Done!"
