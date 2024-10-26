80470b8238a1a5c4835893f6c41caf163845a8cf
Daniel <blt950@users.noreply.github.com>
2024-10-26 12:44:42 +0200

    chore(deps): Updated to node v23 in theme building

--- container/theme/build.sh
+++ container/theme/build.sh
@@ -8,7 +8,7 @@ apt install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-NODE_MAJOR=22
+NODE_MAJOR=23
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 apt update
