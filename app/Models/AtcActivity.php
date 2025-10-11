6e444d5b9b0e92cc3dfe018a4686df28ec7cef78
FabioMike06 <72151591+FabioMike06@users.noreply.github.com>
2025-10-12 00:16:01 +0200

    implements last online & last 12m on roster

--- app/Models/AtcActivity.php
+++ app/Models/AtcActivity.php
@@ -6,12 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AtcActivity extends Model
 {
-    protected $fillable = ['user_id', 'area_id', 'hours', 'start_of_grace_period', 'atc_active'];
+    protected $fillable = ['user_id', 'area_id', 'hours', 'start_of_grace_period', 'atc_active', 'last_online', 'last12Months'];
 
     protected $casts = [
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
         'atc_active' => 'boolean',
+        'last_online' => 'datetime',
         'start_of_grace_period' => 'datetime',
     ];
 
---
+++ database/migrations/2025_10_11_230400_update_atc_activity_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('atc_activities', function (Blueprint $table) {
+            $table->timestamp('last_online')->nullable();
+            $table->double('last12Months')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('atc_activities', function (Blueprint $table) {
+            $table->dropColumn('last_online');
+            $table->dropColumn('last12Months');
+        });
+    }
+};
