88705dac4859db397357886e9c35aaeb7207377c
Thor K. Høgås <thor@roht.no>
2026-01-19 12:15:22 +0100

    fix(theme): use tertiary to avoid poor contrast

--- resources/views/usersettings.blade.php
+++ resources/views/usersettings.blade.php
@@ -8,7 +8,7 @@
 
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 fw-bold text-primary">Settings</h6> 
+                <h6 class="m-0 fw-bold text-tertiary">Settings</h6>
             </div>
             <div class="card-body">
                 <div class="row">
