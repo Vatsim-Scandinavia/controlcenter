82271cab75072272964dc62825ebddbe5747ec9c
Simon Bjerre <simon@bjerrebakholdt.dk>
2026-03-04 20:39:17 +0100

    Resolved linting

--- app/Services/StatisticsService.php
+++ app/Services/StatisticsService.php
@@ -4,8 +4,8 @@ namespace App\Services;
 
 use App\Exceptions\StatisticsApiException;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class StatisticsService
